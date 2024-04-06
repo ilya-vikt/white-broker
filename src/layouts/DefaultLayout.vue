e7b8917426fd75f9e15dcc201b3404386aff0b27
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-04-06 21:03:45 +0300

    add breadcrumbs

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
+import AppBreadcrumbs from '@/share/components/AppBreadcrumbs/AppBreadcrumbs.vue';
 import MainHeader from '@/share/components/headers/MainHeader/MainHeader.vue';
+import { useBreakpoints } from '@/share/composables/useBreakpoints';
+
+const { isCompact } = useBreakpoints();
 </script>
+
 <template>
   <MainHeader />
+  <AppBreadcrumbs
+    v-if="!isCompact"
+    class="container"
+    :crumbs="[
+      {
+        caption: 'Главная',
+        href: '/'
+      },
+      {
+        caption: 'Купить',
+        href: '/'
+      },
+      {
+        caption: 'Квартира',
+        href: '/'
+      }
+    ]"
+  />
   <main>
     <slot />
   </main>
---
+++ src/share/components/AppBreadcrumbs/AppBreadcrumbs.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const props = defineProps<{
+  crumbs: {
+    href: string;
+    caption: string;
+  }[];
+}>();
+
+const crumbsList = computed(() => props.crumbs.slice(0, -1));
+const lastCrumb = computed(() => props.crumbs[props.crumbs.length - 1]);
+</script>
+
+<template>
+  <ul class="breadcrumbs">
+    <li v-for="crumbItem in crumbsList" :key="crumbItem.caption" class="breadcrumbs__item">
+      <RouterLink class="breadcrumbs__crumb lnk" :to="crumbItem.href">
+        {{ crumbItem.caption }}
+      </RouterLink>
+    </li>
+    <li class="breadcrumbs__item">
+      <span class="breadcrumbs__crumb">{{ lastCrumb.caption }}</span>
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="scss" src="./appBreadcrumbs.scss"></style>
