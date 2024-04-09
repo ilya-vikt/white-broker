932ba2cbc6a4cc86f40ad4281c450e37c687ed3d
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-04-10 01:06:02 +0300

    add price formatter function. This function uses in filters

--- src/share/utils/priceFormat.ts
+++ src/share/utils/priceFormat.ts
@@ -6,3 +6,21 @@ export const priceFormat = (price: number): string => {
   });
   return formatter.format(price);
 };
+
+export const shortenNumber = (num: number): string => {
+  const abbreviations = ['', 'тыс', 'млн', 'млрд'];
+  const tier = (Math.log10(num) / 3) | 0;
+
+  if (tier === 0) return num.toString().replace('.', ',');
+
+  const suffix = abbreviations[tier];
+  const scale = Math.pow(10, tier * 3);
+  const scaled = num / scale;
+
+  const formatted = scaled
+    .toFixed(1)
+    .replace(/\.?0*$/, '')
+    .replace('.', ',');
+
+  return formatted + ' ' + suffix;
+};
