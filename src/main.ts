618afada16458b804211585a083d7faae1ae3259
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-04-09 18:25:35 +0300

    add DropdownBase component. This is component is base for dropdown filters

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import '@/assets/styles/main.scss';
 import { register } from 'swiper/element/bundle';
-
+import FloatingVue from 'floating-vue';
+import '@/assets/styles/main.scss';
+import 'floating-vue/dist/style.css';
 register();
 
 const app = createApp(App);
 app.use(router);
+app.use(FloatingVue, {
+  themes: {
+    'info-dropdown': {
+      $extend: 'dropdown',
+      $resetCss: 'true',
+      placement: 'bottom-start',
+      delay: 100
+    }
+  }
+});
 app.mount('#app');
---
+++ src/share/components/DropDownBase/DropDownBase.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{
+  placeholder: string;
+  displayValue: string | null;
+}>();
+
+const isExpanded = ref(false);
+</script>
+
+<template>
+  <VDropdown
+    v-model:shown="isExpanded"
+    class="dropdown-base control-base"
+    :class="{ 'dropdown-base--expanded': isExpanded }"
+    theme="info-dropdown"
+    :distance="8"
+  >
+    <div class="dropdown-base__display" :class="{ 'dropdown-base__display--empty': !displayValue }">
+      {{ displayValue ?? placeholder }}
+    </div>
+    <button class="dropdown-base__trigger" type="button"></button>
+    <template #popper>
+      <slot />
+    </template>
+  </VDropdown>
+</template>
+
+<style scoped lang="scss" src="./dropDownBase.scss"></style>
