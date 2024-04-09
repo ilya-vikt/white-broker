65b9b79f58ecf683d7268f3ef9703629ff24c03f
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-04-09 18:28:09 +0300

    add SelectListFilter component

--- src/catalog/components/RealtyFilters/RealtyFilters.vue
+++ src/catalog/components/RealtyFilters/RealtyFilters.vue
@@ -1,14 +1,69 @@
 <script setup lang="ts">
 import RoomCountFilter from '@/catalog/components/RealtyFilters/blocks/RoomCountFilter/RoomCountFilter.vue';
+import SelectListFilter from '@/catalog/components/RealtyFilters/blocks/SelectListFilter/SelectListFilter.vue';
 import { reactive } from 'vue';
 
 const filters = reactive({
-  roomCount: 0
+  roomCount: 0,
+  houseType: [1],
+  realtyType: 1
 });
+const realtyTypes = [
+  {
+    caption: 'Квартира',
+    value: 1
+  },
+  {
+    caption: 'Дом',
+    value: 2
+  },
+  {
+    caption: 'Коммерция',
+    value: 3
+  }
+];
+
+const houseTypes = [
+  {
+    caption: 'Дом',
+    value: 1
+  },
+  {
+    caption: 'Дача',
+    value: 2
+  },
+  {
+    caption: 'Коттедж',
+    value: 3
+  },
+  {
+    caption: 'Таунхаус',
+    value: 4
+  },
+  {
+    caption: 'Доля в доме',
+    value: 5
+  }
+];
 </script>
 
 <template>
-  <div class="realty-filters"><RoomCountFilter v-model="filters.roomCount" /></div>
+  <div class="realty-filters">
+    <SelectListFilter
+      v-model="filters.realtyType"
+      :filter-data="realtyTypes"
+      :filter-name="'realtyType'"
+      placeholder="Выберите значение"
+    />
+    <RoomCountFilter v-model="filters.roomCount" />
+    <SelectListFilter
+      v-model="filters.houseType"
+      :filter-data="houseTypes"
+      :filter-name="'houseType'"
+      placeholder="Выберите значение"
+      multiselect
+    />
+  </div>
 </template>
 
 <style scoped lang="scss" src="./realtyFilters.scss"></style>
---
+++ src/catalog/components/RealtyFilters/blocks/SelectListFilter/SelectListFilter.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import DropDownBase from '@/share/components/DropDownBase/DropDownBase.vue';
+import { computed } from 'vue';
+
+const props = defineProps<{
+  filterName: string;
+  placeholder: string;
+  filterData: {
+    caption: string;
+    value: number;
+  }[];
+  multiselect?: boolean;
+}>();
+
+const modelValue = defineModel<number[] | number>();
+
+const displayValue = computed(() => {
+  if (!modelValue.value) return null;
+  if (props.multiselect) {
+    return (
+      (modelValue.value as number[])
+        ?.map((el) => props.filterData.find(({ value }) => value === el)?.caption)
+        .join(', ') || null
+    );
+  }
+
+  return (
+    props.filterData.find(({ value }) => value === (modelValue.value as number))?.caption ?? null
+  );
+});
+</script>
+
+<template>
+  <DropDownBase
+    class="select-filter"
+    :display-value="displayValue"
+    placeholder="Значение не выбрано"
+  >
+    <ul class="select-filter__list">
+      <li
+        v-for="data in filterData"
+        :key="data.value"
+        v-close-popper="!multiselect"
+        class="select-filter__item"
+      >
+        <input
+          :id="`id-${data.value}`"
+          v-model="modelValue"
+          :type="multiselect ? 'checkbox' : 'radio'"
+          :value="data.value"
+          :name="filterName"
+        />
+        <label class="lnk" :for="`id-${data.value}`">{{ data.caption }}</label>
+      </li>
+    </ul>
+  </DropDownBase>
+</template>
+
+<style scoped lang="scss" src="./selectListFilter.scss"></style>
