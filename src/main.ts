79a3556bed505fe124a724caa503b5fbce16341c
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-04-09 09:00:56 +0300

    add swiper-slider plugin

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import '@/assets/styles/main.scss';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 const app = createApp(App);
 app.use(router);
