056281ada509b0c8c7e0a6ad496ee813e5812a1a
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-04-10 08:18:29 +0300

    add PriceFilter and AreaFilters to a filter panel.

--- src/catalog/components/RealtyFilters/RealtyFilters.vue
+++ src/catalog/components/RealtyFilters/RealtyFilters.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
 import RoomCountFilter from '@/catalog/components/RealtyFilters/blocks/RoomCountFilter/RoomCountFilter.vue';
 import SelectListFilter from '@/catalog/components/RealtyFilters/blocks/SelectListFilter/SelectListFilter.vue';
+import PriceFilter from '@/catalog/components/RealtyFilters/blocks/PriceFilter/PriceFilter.vue';
+import AreaFilter from '@/catalog/components/RealtyFilters/blocks/AreaFilter/AreaFilter.vue';
+import AppButton from '@/share/components/AppButton/AppButton.vue';
+import { CrossIcon } from '@/share/components/icons';
+import { useBreakpoints } from '@/share/composables/useBreakpoints';
 import { reactive } from 'vue';
 
 const filters = reactive({
   roomCount: 0,
   houseType: [1],
-  realtyType: 1
+  realtyType: 1,
+  price: {
+    priceMin: 300000,
+    priceMax: 100000000,
+    algorithm: 'total'
+  },
+  area: {
+    houseAreaMin: 20,
+    houseAreaMax: 100,
+    landAreaMin: 5,
+    landAreaMax: 100
+  },
+  sort: 1
 });
+
 const realtyTypes = [
   {
     caption: 'Квартира',
@@ -45,24 +63,72 @@ const houseTypes = [
     value: 5
   }
 ];
+
+const sortTypes = [
+  {
+    caption: 'по актуальности',
+    value: 1
+  },
+  {
+    caption: 'цена по возрастанию',
+    value: 2
+  },
+  {
+    caption: 'цена по убыванию',
+    value: 3
+  }
+];
+
+const { isCompact } = useBreakpoints();
 </script>
 
 <template>
   <div class="realty-filters">
-    <SelectListFilter
-      v-model="filters.realtyType"
-      :filter-data="realtyTypes"
-      :filter-name="'realtyType'"
-      placeholder="Выберите значение"
-    />
-    <RoomCountFilter v-model="filters.roomCount" />
-    <SelectListFilter
-      v-model="filters.houseType"
-      :filter-data="houseTypes"
-      :filter-name="'houseType'"
-      placeholder="Выберите значение"
-      multiselect
-    />
+    <div class="realty-filters__top">
+      <div class="realty-filters__inner">
+        <SelectListFilter
+          v-model="filters.realtyType"
+          :filter-data="realtyTypes"
+          :filter-name="'realtyType'"
+          placeholder="Выберите значение"
+        />
+        <RoomCountFilter v-model="filters.roomCount" class="realty-filters__room" />
+        <SelectListFilter
+          v-model="filters.houseType"
+          :filter-data="houseTypes"
+          :filter-name="'houseType'"
+          placeholder="Выберите значение"
+          multiselect
+        />
+        <PriceFilter
+          v-model="filters.price"
+          filter-name="price"
+          :min-val="300000"
+          :max-val="100000000"
+        />
+        <AreaFilter
+          v-model="filters.area"
+          filter-name="price"
+          :min-val="300000"
+          :max-val="100000000"
+        />
+      </div>
+      <AppButton class="realty-filters__btn">Найти</AppButton>
+    </div>
+    <div v-if="!isCompact" class="realty-filters__bottom">
+      <div class="realty-filters__sort">
+        <span>Сортировать:</span>
+        <SelectListFilter
+          v-model="filters.sort"
+          :filter-data="sortTypes"
+          :filter-name="'sortType'"
+          placeholder="Выберите значение"
+          no-border
+        />
+      </div>
+      <span class="realty-filters__founded">Найдено 91 предложение</span>
+      <button class="realty-filters__clear lnk">Очистить фильтр<CrossIcon /></button>
+    </div>
   </div>
 </template>
 
