baa3aa851bd73984b1d8fb5a341c850e191094a9
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-04-10 08:10:58 +0300

    Add NumberInput component. This component is a base for a numeric filters field

---
+++ src/catalog/components/RealtyFilters/blocks/NumberInput/NumberInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const modelValue = defineModel<number>({ default: 0 });
+defineProps<{
+  placeholder: string;
+  unit: string;
+  minVal: number;
+  maxVal: number;
+}>();
+</script>
+
+<template>
+  <label class="number-input control-base">
+    <input
+      v-model="modelValue"
+      class="number-input__control"
+      type="number"
+      :min="minVal"
+      :max="maxVal"
+    />
+    <span class="number-input__unit">{{ unit }}</span>
+  </label>
+</template>
+
+<style scoped lang="scss" src="./numberInput.scss"></style>
